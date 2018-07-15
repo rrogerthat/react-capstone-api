d6629451e21350605d5a0eaf2097e756f05a449f
Roger Hwang <hroger19@gmail.com>
2018-07-15 00:30:39 -0700

    added cors middleware

---
+++ config.js
@@ -0,0 +1,3 @@
+'use strict';
+
+exports.CLIENT_ORIGIN = "*";
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 "use strict";
 
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');	
 
+const cors = require('cors');
+const {CLIENT_ORIGIN} = require('./config');
+
 const app = express();
 app.use(morgan('common'));	
 app.use(bodyParser.json());
+app.use(
+    cors({
+    	origin: CLIENT_ORIGIN
+    })
+);
 
 const PORT = process.env.PORT || 3000;
 
