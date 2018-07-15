e092102875851c5cbe23a992a0a9dfe49f25aea9
Roger Hwang <hroger19@gmail.com>
2018-07-14 23:31:44 -0700

    connected to Travis CI and Heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 "use strict";
 
-require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');	
 
 const app = express();
 app.use(morgan('common'));	
