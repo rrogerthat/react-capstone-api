f19acfd861f0dd7438b3899cbc4a8400e303b3e2
Roger Hwang <hroger19@gmail.com>
2018-07-17 15:42:35 -0700

    changes to test file for Travis CI

--- test/test-server.js
+++ test/test-server.js
@@ -10,7 +10,7 @@ describe('API', function() {
 
   it('should 200 on GET requests', function() {
     return chai.request(app)
-      .get('/api/fooooo')
+      .get('/api/')
       .then(function(res) {
         res.should.have.status(200);
         res.should.be.json;
